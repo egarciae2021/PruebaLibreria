0f2b16d89756e5baa3fa1a1d71ccbb5f78e66aee
egarciae2021 <81602306+egarciae2021@users.noreply.github.com>
2024-08-31 11:52:02 -0500

    Create libreria.js

---
+++ libreria.js
@@ -0,0 +1,16 @@
+// miLibreria.js
+
+// Definir la librería dentro de una IIFE (Immediately Invoked Function Expression)
+(function(global) {
+    // Crear un objeto para la librería
+    const MiLibreria = {};
+
+    // Agregar la función para mostrar el alert
+    MiLibreria.saludar = function() {
+        alert("Hola Mundo");
+    };
+
+    // Exponer la librería en el objeto global (window en un navegador)
+    global.MiLibreria = MiLibreria;
+
+})(this);
