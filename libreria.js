9b27381a079f9c7d2f30952e11b2f188b01e09b3
egarciae2021 <81602306+egarciae2021@users.noreply.github.com>
2024-08-31 12:10:15 -0500

    Update libreria.js

--- libreria.js
+++ libreria.js
@@ -7,7 +7,7 @@
 
     // Agregar la función para mostrar el alert
     MiLibreria.saludar = function() {
-        alert("Hola Mundoasdsdsdss");
+        alert("Pepe lucho");
     };
 
     // Exponer la librería en el objeto global (window en un navegador)
